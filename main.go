96f3f4237ef39dc4e4e12f78b8262544de79296c
James <james.thomas.vaughn@gmail.com>
2019-05-17 22:52:24 -0400

    Have basic dns sniffing working with questions / answers printing to stdout

--- main.go
+++ main.go
@@ -1,23 +1,18 @@
-//https://www.devdungeon.com/content/packet-capture-injection-and-analysis-gopacket
+//Infomation on gopacket https://www.devdungeon.com/content/packet-capture-injection-and-analysis-gopacket
+//Example code https://gist.github.com/dvas0004/3d280f95a83a3084b314ea208f19f9e9
 package main
 
 import (
 	"flag"
-	"fmt"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 	"log"
-	"time"
 )
 
 const (
 	//Amount of packet collected; long enough for headers
-	SNAPSHOT_LEN        = 1024
-	//needed to snoop on the interface
-	PROMISCUOUS_MODE 	= true
-	//time period packets are collected before pushing the results to the Packets channel
-	COLLECTION_INTERVAL = -1 * time.Second
+	SNAPSHOT_LEN        = 4096
 )
 
 var (
@@ -30,16 +25,14 @@ func init() {
 }
 
 func main() {
-	handle, err := pcap.OpenLive(INTERFACE_NAME, SNAPSHOT_LEN, true, COLLECTION_INTERVAL)
+	handle, err := pcap.OpenLive(INTERFACE_NAME, SNAPSHOT_LEN, true, pcap.BlockForever)
 	if err != nil {
 		log.Fatalf("Couldn't open device for capture: %v", err)
 	}
 
 	defer handle.Close()
 
-	if err := capturePackets(handle); err != nil {
-		log.Fatalf("Error encountered while capturing packets: %v", err)
-	}
+	capturePackets(handle);
 }
 
 func parseFlags() {
@@ -47,15 +40,43 @@ func parseFlags() {
 	flag.Parse()
 }
 
-func capturePackets(handle *pcap.Handle) error {
-	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-	for packet := range packetSource.Packets() {
-		// Process packet here
-		dnsLayer := packet.Layer(layers.LayerTypeDNS)
-		if dnsLayer != nil {
-			fmt.Println(packet)
+func capturePackets(handle *pcap.Handle) {
+	//Need all of the layers up to DNS to parse out just DNS
+	var eth layers.Ethernet
+	var ip4 layers.IPv4
+	var ip6 layers.IPv6
+	var tcp layers.TCP
+	var udp layers.UDP
+	var dns layers.DNS
+	var payload gopacket.Payload
+
+	parser := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &eth, &ip4, &ip6, &tcp, &udp, &dns, &payload)
+	decodedLayers := make([]gopacket.LayerType, 0, 10)
+
+	for {
+		data, _, err := handle.ReadPacketData()
+
+		if err != nil {
+			log.Println("Error reading packet data: %w", err)
+			continue
+		}
+
+		parser.DecodeLayers(data, &decodedLayers)
+		for _, layerType := range decodedLayers {
+			switch layerType {
+			case layers.LayerTypeDNS:
+				handleDnsPacket(dns)
+			}
 		}
 	}
+}
 
-	return nil
+func handleDnsPacket(dnsPacket layers.DNS) {
+	for _, question := range dnsPacket.Questions {
+		log.Printf("Question: %s\n", string(question.Name))
+	}
+
+	for _, answer := range dnsPacket.Answers {
+		log.Printf("Answer: %s\n", string(answer.IP.String()))
+	}
 }
